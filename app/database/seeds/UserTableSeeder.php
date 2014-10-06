4a305d2a70fd12dcbd2a1745a2c9f29ddec98eed
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-06 15:37:15 +0200

    add couple of users for testing

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -15,6 +15,27 @@ class UserTableSeeder extends Seeder
             'created_at' => new DateTime(),
             'updated_at' => new DateTime()
         ]);
+
+        DB::table('users')->insert([
+            'username'   => 'thierry',
+            'email'      => 'thierry@rebels.com',
+            'password'   => Hash::make('thierry'),
+            'first_name' => 'thierry',
+            'last_name'  => 'schellenbach',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime()
+        ]);
+
+        DB::table('users')->insert([
+            'username'   => 'tommaso',
+            'email'      => 'tommaso@empire.com',
+            'password'   => Hash::make('tommaso'),
+            'first_name' => 'tommaso',
+            'last_name'  => 'barbugli',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime()
+        ]);
+
     }
 
 }
