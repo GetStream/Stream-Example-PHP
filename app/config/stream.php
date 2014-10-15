f29ff363df491b5e66746a49964de73cfbae339e
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-15 16:41:29 +0200

    cleanup old files

--- app/config/stream.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-	'api_key' => null,
-	'api_secret' => null,
-	'app_id' => null
-);
--- app/views/_activity.blade.php
+++
@@ -1,5 +0,0 @@
-@if ($activity['content_type'] == 'pin')
-    @include('_pin_activity', array('activity'=>$activity))
-@elseif ($activity['content_type'] == 'follow')
-    @include('_follow_activity', array('activity'=>$activity))
-@endif
