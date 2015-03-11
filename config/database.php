0bb810b2bab144abe0e76acb7bdfe00bd956a57e
Mario Delgado <mariodel@gmail.com>
2015-03-11 12:42:28 -0600

    use heroku pgsql

--- config/database.php
+++ config/database.php
@@ -80,10 +80,10 @@ return [
 
 		'pgsql' => [
 			'driver'   => 'pgsql',
-			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'host'     => $host,
+			'database' => $database,
+			'username' => $username,
+			'password' => $password,
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
