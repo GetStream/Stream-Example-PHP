2cee51d61793427b330cc9b4481aedb7a252a323
tbarbugli <tbarbugli@gmail.com>
2016-07-06 11:47:56 +0200

    replace layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,70 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Stream Framework example | Pinterest esque example app</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
 
-        <meta name="HandheldFriendly" content="True">
-        <meta name="MobileOptimized" content="320">
-        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-        <meta http-equiv="cleartype" content="on">
+    <script type="text/javascript">
+    if (window.location.protocol != "http:")
+        window.location.href = "http:" + window.location.href.substring(window.location.protocol.length);
+    </script>
 
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ URL::asset('images/favicon-144x144-precomposed.png') }}">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ URL::asset('images/favicon-114x114-precomposed.png') }}">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ URL::asset('images/favicon-72x72-precomposed.png') }}">
-        <link rel="apple-touch-icon-precomposed" href="{{ URL::asset('images/favicon-57x57-precomposed.png') }}">
-        <link rel="shortcut icon" href="{{ URL::asset('images/favicon.ico') }}">
-        <link rel="logo" type="image/svg" href="{{ URL::asset('images/logo.svg') }}">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
-        <meta name="msapplication-TileImage" content="{{ URL::asset('images/favicon-144x144-precomposed.png') }}">
-        <meta name="msapplication-TileColor" content="#222222">
-        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
-        <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" type="text/x-scss" charset="utf-8">
-        <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,500' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-        @section('nav')
-            @include('_navigation', array('location'=>'trending'))
-        @show
-        <section>
-            <article>
-                @section('info')
-                    <div class="jumbotron">
-                        <div class="container">
-                            <h1>Stream in action</h1>
-                            <p>
-                                Welcome to the Pinterest esque example app. We've conveniently logged you in as the admin user.
-                                Also admin is a slightly narcissistic user and follows her own account.
-                                
-                                When you pin items on this trending page, you'll see them appear in the feeds of your followers.
-                                Pin a few items and have a look at the Flat and Aggregated timelines pages.
-                            </p>
-                            <p>
-                                <a class="btn btn-primary btn-lg" href="https://getstream.io" role="button">
-                                    <i class="icon-help-circle"></i> GetStream
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                @show
+    <!-- Fonts -->
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
-                @yield('content')
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Laravel</a>
+            </div>
 
-            </article>
-        </section>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/') }}">Home</a></li>
+                </ul>
 
-        <script type="text/javascript">
-            var queries = {{ json_encode(DB::getQueryLog()) }};
-            console.log('/****************************** Database Queries ******************************/');
-            console.log(' ');
-            $.each(queries, function(id, query) {
-                console.log('   ' + query.time + ' | ' + query.query);
-            });
-            console.log(' ');
-            console.log('/****************************** End Queries ***********************************/');
-        </script>
-    </body>
+                <ul class="nav navbar-nav navbar-right">
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                        <li><a href="{{ url('/auth/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    @yield('content')
+
+    <!-- Scripts -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</body>
 </html>
